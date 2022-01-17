0c3a1292c617ad79d2d19a0a0e1eb529a01195d1
JRRS1982 <jeremyrrsmith@gmail.com>
2022-01-17 08:24:28 +0000

    ADD EsLint, Prettier, Husky, tsconfig-paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   verbose: true,
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  roots: ["<rootDir>/src/"],
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  roots: ['<rootDir>/src/'],
   moduleNameMapper: {
     // TODO any other modules need to be mapped?
-    "^Actions(.*)$": "<rootDir>/src/Actions/$1",
-    "^Configs(.*)$": "<rootDir>/src/Configs/$1",
-    "^Entities(.*)$": "<rootDir>/src/Entities/$1",
-    "^Resolvers(.*)$": "<rootDir>/src/Resolvers/$1",
-    "^Repositories(.*)$": "<rootDir>/src/Repositories/$1",
-  },
+    '^Actions(.*)$': '<rootDir>/src/Actions/$1',
+    '^Configs(.*)$': '<rootDir>/src/Configs/$1',
+    '^Entities(.*)$': '<rootDir>/src/Entities/$1',
+    '^Resolvers(.*)$': '<rootDir>/src/Resolvers/$1',
+    '^Repositories(.*)$': '<rootDir>/src/Repositories/$1'
+  }
 };
 
 export default config;
