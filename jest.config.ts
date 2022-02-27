80dd365f1c9efe5ee0d3050a4404f3963cb1bc3b
JRRS1982 <jeremyrrsmith@gmail.com>
2022-02-27 14:51:05 +0000

    ADD clear and reset mocks to jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -7,13 +7,15 @@ const config: Config.InitialOptions = {
   roots: ['<rootDir>/src/'],
   setupFiles: ['dotenv/config'], // required by jest to load dotenv config
   moduleNameMapper: {
-    // TODO any other modules need to be mapped? And get the short path names compiling as not currently successful.
     '^Actions(.*)$': '<rootDir>/src/Actions/$1',
     '^Configs(.*)$': '<rootDir>/src/Configs/$1',
     '^Entities(.*)$': '<rootDir>/src/Entities/$1',
     '^Resolvers(.*)$': '<rootDir>/src/Resolvers/$1',
-    '^Repositories(.*)$': '<rootDir>/src/Repositories/$1'
-  }
+    '^Repositories(.*)$': '<rootDir>/src/Repositories/$1',
+    '^Services(.*)$': '<rootDir>/src/Services/$1'
+  },
+  clearMocks: true, // clear calls, instances and results between each test
+  resetMocks: true // reset mock state between each test
 };
 
 export default config;
